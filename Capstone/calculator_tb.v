d9c7c8b4ca60a848fd8d25e84870f80d152a7795
Porg0 <alejandro.bonilla925@gmail.com>
2023-11-20 11:49:51 -0600

    Fixed Testbench

--- Capstone/calculator_tb.v
+++ Capstone/calculator_tb.v
@@ -4,9 +4,10 @@
 
 module testbench ();
     reg Button1,Button2,Equals,Reset,Operation;
-    wire [6:0] S7D1,S7D2,S7D3,S7D4;
+    wire [6:0] Dig1, Dig2;
+    wire[3:0] Feed1, Feed2, Feed3, Feed4;
 
-    top UUT (Button1,Button2,Equals,Reset,Operation,S7D1,S7D2,S7D3,S7D4);
+    top UUT (Button1,Button2,Equals,Reset,Operation,Dig1,Dig2);
 
     //Button Loops
 
@@ -18,6 +19,7 @@ module testbench ();
         always begin
             #10 Button2 = ~Button2;
         end
+    
     //Driver module
     initial begin
         $dumpfile("calculator.vcd");
@@ -28,15 +30,20 @@ module testbench ();
         Operation=0; Equals=0; Reset=0; #(10);
         Operation=0; Equals=0; Reset=0; #(10);
         Operation=0; Equals=0; Reset=0; #(10);
+        Operation=0; Equals=0; Reset=0; #(10);
+        Operation=0; Equals=0; Reset=0; #(10);
+        Operation=0; Equals=0; Reset=0; #(10);
+        Operation=0; Equals=0; Reset=0; #(10);
+        Operation=0; Equals=0; Reset=0; #(10);
+        Operation=0; Equals=0; Reset=0; #(10);
         #(10*10);
         Operation=0; Equals=0; Reset=0; #(10);
         Operation=1; Equals=0; Reset=0; #(10);
-        /*Operation=0; Equals=0; Reset=0; #(10);
         Operation=0; Equals=0; Reset=0; #(10);
-        Operation=0; Equals=0; Reset=0; #(10);
+        Operation=0; Equals=0; Reset=1; #(10);
+        Operation=0; Equals=1; Reset=0; #(10);
         #(10*10);
-        Operation=1; Equals=1; Reset=0; #(10);*/
+        Operation=1; Equals=0; Reset=0; #(10);
         $finish;
     end
-
 endmodule
